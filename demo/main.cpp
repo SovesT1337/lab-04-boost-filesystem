453420ac6dad5279d8078c5933021b9706552ec2
SovesT1337 <temirkhan2002@gmail.com>
2021-11-24 10:20:41 +0300

    VII Commit

--- demo/main.cpp
+++ demo/main.cpp
@@ -1,11 +1,12 @@
 #include <magic.hpp>
 
-int main(int argc, char* argv[]) {
-  path p;
-  if (argc >= 2)
-    p = argv[1];
-  else
-    p = boost::filesystem::current_path();
+int main() {
+  // int main(int argc, char* argv[]) {
+  path p = "/home/sovest/CLionProjects/lab-04-boost-filesystem/misc";
+  // if (argc >= 2)
+  //   p = argv[1];
+  // else
+  //   p = boost::filesystem::current_path();
   vector<file> list;
   walker(p, list);
   print(list);
